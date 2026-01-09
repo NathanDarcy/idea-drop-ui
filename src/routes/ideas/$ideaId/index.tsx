620d1a092c252c07771df79848b497bc3a3ef9b9
Nathan Darcy <nathandarcy33@gmail.com>
2026-01-09 11:43:46 -0600

    add simple loader for api call - Nathan

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,12 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+async function fetchIdea(ideaId: string) {
+  const response = await fetch(`http://localhost:8000/ideas/${ideaId}`)
+  if (!response.ok) {
+    throw new Error('Failed to fetch')
+  }
+
+  return response.json()
+}
+
 export const Route = createFileRoute('/ideas/$ideaId/')({
   head: () => ({
     meta: [{ title: 'IdeaHub - Browse Ideas' }],
   }),
   component: IdeaDetailsPage,
+  loader: async ({ params }) => {
+    return fetchIdea(params.ideaId)
+  },
 })
 
 function IdeaDetailsPage() {
-  return <div>Hello "/ideas/$ideaId/"!</div>
+  const idea = Route.useLoaderData()
+
+  return <div>{idea.title}</div>
 }
